56fa6e17f85c712f5f5a6b018b35051a63200e35
Andrew Shopland <hi@ashp.land>
2017-10-02 17:54:09 -0700

    Initial Commit

---
+++ WordEffects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WordEffects
+//
+//  Created by Andrew on 2017-10-02.
+//  Copyright © 2017 Andrew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
